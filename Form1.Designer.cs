77c16f21a426ba76a9c1b0f05862e9134cff0059
David Latimer <davidlat2005@gmail.com>
2023-02-27 22:30:09 +0000

    and god said 'let there be dithering'

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -17,25 +17,28 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
         ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
+        ///  the contents of this method with the code editor. <summary>
+
+        // This sign can't stop me because i can't read
         private void InitializeComponent()
         {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             SuspendLayout();
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(960, 540);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Rendering Engine";
             Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace Rendering_Engine_NEA_Project
 {
     public partial class Form1 : Form
@@ -7,8 +9,35 @@ namespace Rendering_Engine_NEA_Project
             InitializeComponent();
         }
 
+        private PictureBox pictureBox1 = new PictureBox();
+        private Font fnt = new Font("Helvetica", 18);
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.Controls.Add(pictureBox1);
+        }
+
+        private void pictureBox1_Paint(object? sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawString("This is a diagonal line drawn on the control", fnt, System.Drawing.Brushes.Blue, new Point(200, 200));
+            // g.DrawLine(new Pen(Color.DarkRed), pictureBox1.Left, pictureBox1.Top, pictureBox1.Right, pictureBox1.Bottom);
+            
+            Brush aBrush = new SolidBrush(Color.DarkRed);
+
+            for (int y = 0; y < 540; y++)
+            {
+                for (int x = 0; x < 960; x++)
+                {
+                    if (x % 2 == 0 && y % 2 == 0)
+                    {
+                        e.Graphics.FillRectangle(aBrush, x, y, 1, 1);
+                    }
+                }
+            }
 
         }
     }
