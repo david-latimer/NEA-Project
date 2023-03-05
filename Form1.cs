e0abd9d069d7ea5928fc270b2fa3e50f116c6951
David Latimer <davidlat2005@gmail.com>
2023-03-05 03:06:11 +0000

    I HAVE A PERSPECTIVE 1ST PERSON VIEW, BLESSED BE

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using System.Numerics;
 
 namespace Rendering_Engine_NEA_Project
 {
@@ -12,6 +13,30 @@ namespace Rendering_Engine_NEA_Project
         private PictureBox pictureBox1 = new PictureBox();
         private Font fnt = new Font("Helvetica", 18);
 
+        string[] room = new string[10] {
+                            "###############",
+                            "#-------------#",
+                            "#---##--------#",
+                            "#---##--------#",
+                            "#---------#---#",
+                            "#---------#---#",
+                            "#--c----------#",
+                            "#---------#---#",
+                            "#-------------#",
+                            "###############"
+                           };
+
+        private char FindGridChar(Vector2 pos, string[] room)
+        {
+            float x = pos.X;
+            float y = pos.Y;
+
+            int new_x = ((int)(x / 10));
+            int new_y = 9 - ((int)(y / 10));
+
+            return room[new_y][new_x];
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             pictureBox1.Dock = DockStyle.Fill;
@@ -23,95 +48,103 @@ namespace Rendering_Engine_NEA_Project
         private void pictureBox1_Paint(object? sender, System.Windows.Forms.PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            // g.DrawString("This is a diagonal line drawn on the control", fnt, System.Drawing.Brushes.Blue, new Point(200, 200));
-            // g.DrawLine(new Pen(Color.DarkRed), pictureBox1.Left, pictureBox1.Top, pictureBox1.Right, pictureBox1.Bottom);
-
-            Brush aBrush = new SolidBrush(Color.DarkRed);
-            Brush bBrush = new SolidBrush(Color.DarkGray);
-
-            // cool lines
-            /*for (int y = 0; y < 540; y++)
-            {
-                for (int x = 0; x < 960; x++)
-                {
-                    if ((y / 5) % 2 == 0)
-                    {
-                        e.Graphics.FillRectangle(aBrush, x, y, 1, 1);
-                    }
-                    else
-                    {
-                        e.Graphics.FillRectangle(bBrush, x, y, 1, 1);
-                    }
-                }
-            }*/
 
             // background
             for (int y = 0; y < 540; y++)
             {
                 for (int x = 0; x < 960; x++)
                 {
-                    e.Graphics.FillRectangle(bBrush, x, y, 1, 1);
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Black), x, y, 1, 1);
                 }
             }
 
-            // propose the camera is at (0, 0, 0)
-            // propose, also, that there happens to be a square at (-2, 3, 6) with a side length 5
-            // how would one render that?
+            // g.DrawString("This is a diagonal line drawn on the control", fnt, System.Drawing.Brushes.Blue, new Point(200, 200));
+            // g.DrawLine(new Pen(Color.White), pictureBox1.Left, pictureBox1.Top, pictureBox1.Right, pictureBox1.Bottom);
 
-            //        ___________
-            //       /     |
-            //      /      |
-            //     /       |
-            //    /        |  T
-            //   /         |
-            //  /          |
-            // C           |
-            //  \          |
-            //   \         |
-            //    \        |
-            //     \       |
-            //      \      |
-            //       \_____|_____
-            //
-            // very hi-tec drawing
+            Brush aBrush = new SolidBrush(Color.White);
 
-            // propose F, the distance from the camera to the 3D plane you are projecting onto
-            // in the following calculations, all x,y,z co-ords refer to the square's position, as the camera is at the origin
 
-            // new X = X * (F/Z) 
-            // new Y = Y * (F/Z)
+            // Next project: Raycasting
 
-            // a 10 side length square at z = 5 would fill the y axis completely
-            // therefore, the size of our cube in pixels must be adjusted for
-            // (540 / 2) * 1.2 = 324
+            // A map for the room
 
-            // at an F value of 5, the projected square would have 2D co-ords of (-1.67, 2.5)
-            // assume a full FOV of 90 degrees
-            // at a screen size of 960x540, that translates to screen co-ords of roughly (320, 135)
+            // "###############"
+            // "#-------------#"
+            // "#---##--------#"
+            // "#---##--------#"
+            // "#---------#---#"
+            // "#---------#---#"
+            // "#--c----------#"
+            // "#---------#---#"
+            // "#-------------#"
+            // "###############"
 
-            // for some reason, the size of a square is determined from the top left vertex, that stays constant while it grows away from it
-            // therefore we must offset the x and y by half the square side length
+            // The # is a wall tile, the - is empty space, and the c is the camera's position
+            // Each tile is 10 units wide and tall, making the grid 100ux100u
 
-            // let's render
-            e.Graphics.FillRectangle(aBrush, 320 - 162, 135 - 162, 324, 324);
+            // The bottom left position is (0, 0)
+            // The top left position is (0, 100)
+            // The bottom right position is (100, 0)
+            // The top right position is (100, 100)
 
-            // The scene set up in Unity:
-            // https://cdn.discordapp.com/attachments/625378398503698438/1079925549771792445/image.png
-            // https://cdn.discordapp.com/attachments/625378398503698438/1079925714658279454/image.png
+            // The camera is currently at (35, 35)
+            // At start, the camera aims due right
 
-            // The output
-            // https://cdn.discordapp.com/attachments/625378398503698438/1079925790688428042/image.png
+            Vector2 cameraPosition = new Vector2(0, 0);
+            
+            // Find camera
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if (room[y][x] == 'c')
+                    {
+                        cameraPosition.X = x * 10 + 5;
+                        cameraPosition.Y = 95 - y * 10;
+                        goto break_out;
+                    }
+                }
+            }
 
-            // I'd say that's cause enough for celebration
+            break_out:
+            float step = 0.2f;
+            float[] heights = new float[960];
 
-            //  _\|/ ^
-            //   (_oo /
-            //  /-|--/
-            //  \ |
-            //    / -i
-            //   /   L
-            //   L
-            // WOOOOOOOOOOOOOOOOOOO
+            for (int ray = 0; ray < 960; ray++)
+            {
+                Vector2 ray_pos = cameraPosition;
+                float length = 0f;
+
+                while (true)
+                {
+                    if (FindGridChar(ray_pos, room) == '#')
+                    {
+                        heights[ray] = length;
+                        break;
+                    }
+
+                    length += step;
+                    float angle = ((float)Math.PI / 180f) * (45f - 45f * ((float)(ray) / 480f));
+                    float new_x = ray_pos.X + (float)Math.Cos(angle) * step;
+                    float new_y = ray_pos.Y + (float)Math.Sin(angle) * step;
+                    ray_pos = new Vector2(new_x, new_y);
+                }
+            }
+
+            // draw heights from bottom
+            for (int i = 0; i < 960; i++)
+            {
+                // int pixels = 540 * (int)(141.42f - heights[i]);
+                int pixels = (int)(540 - (540 * (heights[i]) / 150f));
+                int offset = (540 - pixels) / 2;
+                for (int p = offset; p < pixels + offset; p++)
+                {
+                    e.Graphics.FillRectangle(aBrush, i, p, 1, 1);
+                }
+
+            }
+
+            // draw heights centered
         }
     }
 }
